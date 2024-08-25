de803ff50b55e5be0a9afd4fe9ef9760ea73a836
Damyres <damyresmaciell@gmail.com>
2024-08-25 19:51:40 -0300

    Criar função para adicionar os dados no gráfico

--- script/script.js
+++ script/script.js
@@ -3,28 +3,36 @@ const graficoDolar = document.getElementById('graficoDolar')
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Dólar',
+            data: [],
             borderWidth: 1
         }]
     },
 });
 
 setInterval(() => conectaApi(), 5000)
-async function  conectaApi() {
-        const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
-        const conectaTraduzido = await conecta.json()
-        console.log(conectaTraduzido)
+async function conectaApi() {
+    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+    const conectaTraduzido = await conecta.json()
+    let tempo = geraHorario()
+    let valor = conectaTraduzido.USDBRL.ask
+    adicionardados(graficoParaDolar, tempo, valor)
 }
 
-function geraHorario(){
+function geraHorario() {
     let data = new Date()
     let horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds()
     console.log(horario)
     return horario
 }
 
-
+function adicionardados(grafico, legenda, dados) {
+    grafico.data.labels.push(legenda)
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados)
+    })
+    grafico.update()
+}
 
